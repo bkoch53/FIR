151a824ecce9a9d47e2b4049d80046d640a63b5e
Brandon Koch <bkoch53@gmail.com>
2019-12-08 17:48:40 -0800

    reventing that

--- findings/apps.py
+++ findings/apps.py
@@ -7,5 +7,5 @@ class FindingsConfig(AppConfig):
     def ready(self):
         from fir_plugins.links import registry
         from django.conf import settings
-        registry.register_reverse_link(settings.FINDING_ID_PREFIX + "(\d+)", 'findings:details',
-                                       model='findings.Finding', reverse=settings.FINDING_ID_PREFIX + "{}")
+        registry.register_reverse_link(settings.EVENT_ID_PREFIX + "(\d+)", 'findings:details',
+                                       model='findings.Finding', reverse=settings.EVENT_ID_PREFIX + "{}")
